1111dc1fd72b679fee7f584ad7b651dc0847903a
davidbcnguyen <101545438+davidbcnguyen@users.noreply.github.com>
2022-03-27 14:26:09 -0700

    Cleans up the code

--- Classes/Game.js
+++ Classes/Game.js
@@ -4,24 +4,26 @@ import Board from "./Board.js";
 const prompt = Prompt({ sigint: true });
 const players = ["O", "X"];
 export default class Game {
-    board;
-    turn;
+    #board;
+    #turn;
     
     constructor() {
-        this.board = new Board(6, 7, 4);
-        this.turn = 0;
+        this.#board = new Board(6, 7, 4);
+        this.#turn = 0;
     }
 
     play() {
-        while (!this.board.isOver) {
+        while (!this.#board.isOver) {
             let column;
             do {
-                this.board.print(1);
-                column = prompt(`Player ${this.turn + 1}: `);
-            } while (!isNaN(Number(column)) && !this.board.move(players[this.turn], Number(column) - 1));
-            this.turn = (this.turn + 1) % players.length;
+                console.clear();
+                this.#board.print(1);
+                column = prompt(`Player ${this.#turn + 1}: `);
+            } while (!isNaN(Number(column)) && !this.#board.move(players[this.#turn], Number(column) - 1));
+            this.#turn = (this.#turn + 1) % players.length;
         }
-        this.board.print(1);
-        console.log(`${players[(this.turn + 1) % players.length]} wins!`);
+        console.clear();
+        this.#board.print(1);
+        console.log(`${players[(this.#turn + 1) % players.length]} wins!`);
     }
 }
